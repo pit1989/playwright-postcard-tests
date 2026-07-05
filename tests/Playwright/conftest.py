e9359d749f0998fd47292ce96c774757cc126353
Владимир <manrock1989@mail.ru>
2026-07-05 09:02:26 +0300

    refactor: drop pytest-playwright plugin to enforce pure sync execution

--- tests/Playwright/conftest.py
+++ tests/Playwright/conftest.py
@@ -1,13 +1,28 @@
+import os
 import pytest
+from playwright.sync_api import sync_playwright
 from pages.postcard_page import PostcardPage
 
+@pytest.fixture(scope="session")
+def playwright_instance():
+    playwright = sync_playwright().start()
+    yield playwright
+    playwright.stop()
+
+@pytest.fixture(scope="session")
+def browser(playwright_instance):
+    is_ci = os.getenv("CI") == "true"
+    browser = playwright_instance.chromium.launch(headless=is_ci)
+    yield browser
+    browser.close()
+
 @pytest.fixture(scope="function")
-def postcard(page):
-    # Фикстура 'page' предоставляется плагином pytest-playwright автоматически!
-    # Она сама создается в фоновом режиме (headless в CI и с окном локально).
+def postcard(browser):
+    context = browser.new_context()
+    page = context.new_page()
     postcard = PostcardPage(page)
     postcard.open()
     yield postcard
-    # Закрывать страницу вручную не нужно, плагин сделает это сам
+    context.close()
 
 
